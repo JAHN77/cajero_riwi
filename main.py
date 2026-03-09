f7ae10e3d94bb7fb1c30c4a0daa73ca6787f75a9
Juan Andres HN <juanandres072004@hotmail.com>
2026-03-09 13:14:44 -0500

    estructura inicial

---
+++ main.py
@@ -0,0 +1,6 @@
+def ejecutar_programa():
+    print("esta funcional") # Debe tener 4 espacios de sangría
+
+if __name__ == "__main__":
+    ejecutar_programa() # Debe estar pegado al borde izquierdo
+
